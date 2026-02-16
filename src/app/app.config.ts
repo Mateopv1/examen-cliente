349b7a78e087eee380416ef14c93c46344c9dcfa
MATEO PAREDES <mateo_2012@hotmail.es>
2026-02-16 18:18:16 +0100

    ultimas modfifocaciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // <--- ESTO ES LO QUE TE FALTA
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient() // <--- OBLIGATORIO PARA QUE FUNCIONE EL SERVICIO
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
- import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'; // <--- ASEGÚRATE DE QUE ESTA RUTA ES CORRECTA
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
